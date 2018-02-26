e1b2e16004034719820be2fed0e02562cebae31f
Lorand Vaida <lorand.vaida@msg.group>
2018-02-26 10:49:16 +0100

    first commit on jdbc

--- src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShopApplication {
-
+	// this is for jdbc branch
 	public static void main(String[] args) {
 		SpringApplication.run(ShopApplication.class, args);
 	}
